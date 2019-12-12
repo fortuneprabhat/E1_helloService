b6784bec737962b818e3b0603a6366ce5789cab5
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 12:43:10 +0530

    service 1 hello service

---
+++ src/main/java/com/helloService/E1_helloService/E1HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.helloService.E1_helloService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1HelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloService/E1_helloService/E1HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloService.E1_helloService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
