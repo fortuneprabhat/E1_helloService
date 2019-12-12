3673ee873775b79265287d389554d6dfe688e1f3
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 14:17:26 +0530

    enable service as discovery client and exposing API.

--- src/main/java/com/helloService/E1_helloService/E1HelloServiceApplication.java
+++ src/main/java/com/helloService/E1_helloService/E1HelloServiceApplication.java
@@ -2,12 +2,22 @@ package com.helloService.E1_helloService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class E1HelloServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(E1HelloServiceApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String hello() {
+		return "hello Service";
 	}
 
 }
